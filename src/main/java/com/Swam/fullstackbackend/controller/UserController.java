49069772cb56ff498e5e3f3fe79c9b456c1a2480
Swamh <58577548+Swamh@users.noreply.github.com>
2023-08-16 13:34:47 +0800

    Initial commit

---
+++ src/main/java/com/Swam/fullstackbackend/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.Swam.fullstackbackend.controller;
+
+import com.Swam.fullstackbackend.model.User;
+import com.Swam.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("/localhost:3000")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+
+    }
+}
