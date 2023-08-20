b09e9cf342850c3a714bd6a9c36cce6f6470423b
Swamh <58577548+Swamh@users.noreply.github.com>
2023-08-20 15:04:56 +0800

    Update

--- src/main/java/com/Swam/fullstackbackend/controller/UserController.java
+++ src/main/java/com/Swam/fullstackbackend/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.Swam.fullstackbackend.controller;
 
+import com.Swam.fullstackbackend.exception.UserNotFoundException;
 import com.Swam.fullstackbackend.model.User;
 import com.Swam.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
-@CrossOrigin("/localhost:3000")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -20,5 +22,29 @@ public class UserController {
     List<User> getAllUsers(){
         return userRepository.findAll();
 
+    }
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user ->{
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id "+id+" has been deleted success.";
     }
 }
